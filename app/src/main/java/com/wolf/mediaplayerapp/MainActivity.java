3e0413f800270d40f718c695f715e79d1b5fccc6
Wolfof420Street <billodida420@gmail.com>
2019-08-05 02:22:49 +0300

    initial design and basic functionality

--- app/src/main/java/com/wolf/mediaplayerapp/MainActivity.java
+++ app/src/main/java/com/wolf/mediaplayerapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.wolf.mediaplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.create(getApplicationContext(), R.raw.na_iwake);
+
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()) {
+                    pauseMusic();
+                } else {
+                    startMusic();
+                }
+            }
+        });
+    }
+
+    public void pauseMusic () {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            playButton.setText("play");
+        }
+    }
+    public void startMusic () {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            playButton.setText("Pause");
+        }
     }
 }
